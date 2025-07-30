0723821036b47e049b48836e02cdfeecd4f461f1
kisutoo <malk-om@outlook.fr>
2025-07-30 14:53:46 +0200

    menu burger menu langues fini, add scroll progressbar, add page Flash

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,31 @@
 let burgerIcon = document.querySelector(".burger")
 let popupBurger = document.querySelector(".menu-burger")
 let closeBurger = document.querySelector(".croixCloseBurger")
+let closeLangue = document.querySelector(".croixCloseLangue")
+let langue = document.querySelector(".langue")
+let popupLangue = document.querySelector(".selectLangue")
 
 burgerIcon.addEventListener("click", () => {
-    popupBurger.classList.toggle("letVisible")
+    popupBurger.classList.toggle("letVisibleBurger")
 })
 
 closeBurger.addEventListener("click", () => {
-    popupBurger.classList.toggle("letVisible")
+    popupBurger.classList.toggle("letVisibleBurger")
 })
+
+langue.addEventListener("click", () => {
+    popupLangue.classList.toggle("letVisibleLangue")
+})
+
+closeLangue.addEventListener("click", () => {
+    popupLangue.classList.toggle("letVisibleLangue")
+})
+
+function myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("progress-bar").style.width = scrolled + "%";
+}
+
+window.onscroll = function() {myFunction()};
