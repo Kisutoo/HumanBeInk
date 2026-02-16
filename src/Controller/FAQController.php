b84efec009243ac863a4dff8da0c5e822a9c8437
Malkom <malk-om@outlook.fr>
2026-02-16 17:42:09 +0100

    Création page FAQ

---
+++ src/Controller/FAQController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class FAQController extends AbstractController
+{
+    #[Route('/faq', name: 'app_faq')]
+    public function index(): Response
+    {
+        return $this->render('faq/index.html.twig', [
+
+            "logoNom" => 1,
+            "footer" => 1,
+        ]);
+    }
+}
